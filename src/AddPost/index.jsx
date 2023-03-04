6349cb8c6b835ebea66e2af5d9d1f446944d58e7
admin <edelvarden@gmail.com>
2023-03-04 02:38:47 +0200

    add undu function

--- src/AddPost/index.jsx
+++ src/AddPost/index.jsx
@@ -1,68 +1,105 @@
-import { useState } from "react";
-import Input from "../Input";
-import TextArea from "../Textarea";
+import { useCallback, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 function AddPost() {
-    const [postTitle, setPostTitle] = useState("");
-    const [postContent, setPostContent] = useState("");
-
-    function handleTitleChange(event) {
-        setPostTitle(event.target.value);
-    }
-
-    function handleContentChange(event) {
-        setPostContent(event.target.value);
-    }
-
-    const addTag = (tag) => {
-        const selectionStart = document.getElementById("postContent").selectionStart;
-        const selectionEnd = document.getElementById("postContent").selectionEnd;
-        const selectedString = postContent.slice(selectionStart, selectionEnd);
-
-        if (selectedString.length > 0) {
-            const boldString = `<${tag}>${selectedString}</${tag}>`;
-            const newContent =
-                postContent.slice(0, selectionStart) +
-                boldString +
-                postContent.slice(selectionEnd, postContent.length);
-            setPostContent(newContent);
-        }
+  // state variables using useState and ES6 destructuring
+  const [postTitle, setPostTitle] = useState("");
+  const [postContent, setPostContent] = useState("");
+  const [undoStack, setUndoStack] = useState([]);
+  
+  // useCallback to prevent unnecessary re-renders (memoization)
+  const saveToUndoStack = useCallback(
+    (content) => {
+      setUndoStack((prevState) => [...prevState, content]);
+    },
+    [setUndoStack]
+  );
+  
+  const handleUndo = useCallback(
+    (event) => {
+      if (
+        event.ctrlKey &&
+        event.key.toLowerCase() === "z" &&
+        undoStack.length > 0
+      ) {
+        setPostContent(undoStack.pop());
+        setUndoStack([...undoStack]); // force re-render since we're modifying the state directly
+      }
+    },
+    [undoStack]
+  );
+  
+  useEffect(() => {
+    document.addEventListener("keydown", handleUndo);
+    return () => {
+      document.removeEventListener("keydown", handleUndo);
     };
+  }, [handleUndo]);
+  
+  const handleTitleChange = event => {
+    setPostTitle(event.target.value);
+  }
+  
+  const handleContentChange = event => {
+    setPostContent(event.target.value);
+  }
+  
+  // addTag function
+  const addTag = tag => {
+    const selectionStart = document.getElementById("postContent").selectionStart;
+    const selectionEnd = document.getElementById("postContent").selectionEnd;
+    const selectedString = postContent.slice(selectionStart, selectionEnd);
 
-    function addList(tag) {
-        const selectionStart = document.getElementById("postContent").selectionStart;
-        const selectionEnd = document.getElementById("postContent").selectionEnd;
-        const selectedString = postContent.slice(selectionStart, selectionEnd);
-
-        if (selectedString.length > 0) {
-            const boldString = `<${tag}>\n${selectedString.split('\n').map(item => `<li>${item}</li>`).join('\n')}\n</${tag}>`;
-            const newContent =
-                postContent.slice(0, selectionStart) +
-                boldString +
-                postContent.slice(selectionEnd, postContent.length);
-            setPostContent(newContent);
-        }
+    if (selectedString.length > 0) {
+      const boldString = `<${tag}>${selectedString}</${tag}>`;
+      const newContent =
+        postContent.slice(0, selectionStart) +
+        boldString +
+        postContent.slice(selectionEnd, postContent.length);
+      saveToUndoStack(postContent);
+      setPostContent(newContent);
     }
+  };
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        // Code to submit the blog post to a server or database
+  // addList function with ES6 arrow function and template literals 
+  const addList = tag => {
+    const selectionStart = document.getElementById("postContent").selectionStart;
+    const selectionEnd = document.getElementById("postContent").selectionEnd;
+    const selectedString = postContent.slice(selectionStart, selectionEnd);
+
+    if (selectedString.length > 0) {
+      const boldString = `<${tag}>\n${
+        selectedString.split("\n").map(item => `<li>${item}</li>`).join("\n")
+      }\n</${tag}>`;
+      const newContent =
+        postContent.slice(0, selectionStart) +
+        boldString +
+        postContent.slice(selectionEnd, postContent.length);
+      saveToUndoStack(postContent);
+      setPostContent(newContent);
     }
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // Code to submit the blog post to a server or database
+  }
+
 
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="postTitle"></label>
-            <Input
-                label="Title:"
+            <label className={styles.label}>Title</label>
+            <input
+                className={styles.input}
                 type="text"
                 id="postTitle"
                 value={postTitle}
                 onChange={handleTitleChange}
-                placeholder=""
+                required
             />
-            <label htmlFor="postContent">Content:</label>
-            <TextArea
+            <label className={styles.label}>Content</label>
+            <textarea
+                className={styles.textarea}
                 id="postContent"
                 value={postContent}
                 onChange={handleContentChange}
@@ -70,8 +107,11 @@ function AddPost() {
             <button className={styles.button} type="button" onClick={() => addTag("b")}>
                 Bold
             </button>
+            <button className={styles.button} type="button" onClick={() => addTag("i")}>
+                Italic
+            </button>
             <button className={styles.button} type="button" onClick={() => addTag("h2")}>
-                Title (h2)
+                Title
             </button>
             <button className={styles.button} type="button" onClick={() => addList("ul")}>
                 Markered List
@@ -80,6 +120,7 @@ function AddPost() {
                 Numbered List
             </button>
             <button type="submit">Publish</button>
+            <h2>Preview:</h2>
             <div dangerouslySetInnerHTML={{ __html: `<h1>${postTitle}</h1> ${postContent}` }}></div>
         </form>
     );
