de54213da869a221d83b96763ddd252bce933af7
admin <edelvarden@gmail.com>
2023-03-10 22:09:16 +0200

    contact page form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,6 +4,22 @@ const Contact = () => {
     <>
       <div className="contact__container">
         <h1 className="contact__title">Contact</h1>
+        <p>Have any questions or comments? We'd love to hear from you! Fill out the form below and we'll get back to you as soon as possible.</p>
+        <form className="contact__form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input className="form-input" type="text" id="name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input className="form-input" type="email" id="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea className="form-textarea" id="message"></textarea>
+        </div>
+        <button className="form__submit" type="submit">Send</button>
+      </form>
       </div>
     </>
   );
