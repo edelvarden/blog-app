c49a15b2283fbd307377aae6f26c9383aff5f0d2
admin <edelvarden@gmail.com>
2023-03-12 01:02:51 +0200

    added close modal on Escape or empty space click

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -1,49 +1,71 @@
-import { useEffect, useState } from 'react';
-import useDateFormatter from '../../hooks/useDateFormatter';
-import FormButton from '../FormButton';
-import PostEditor from '../PostEditor';
-import './styles.scss';
+import { useEffect, useState } from "react";
+import useDateFormatter from "../../hooks/useDateFormatter";
+import FormButton from "../FormButton";
+import PostEditor from "../PostEditor";
+import "./styles.scss";
 
-const BlogContent = ({ id, title, date, content, image }) => {
-  const [showEditor, setShowEditor] = useState(false);
+// Enumerate the different states of the component
+const BlogContentState = {
+  VIEW: "view",
+  EDIT: "edit",
+};
+
+const BlogContent = React.memo(({ id, title, date, content, image }) => {
+  const [state, setState] = useState(BlogContentState.VIEW);
   const [blogTitle, setBlogTitle] = useState(title);
   const [blogContent, setBlogContent] = useState(content);
 
   const handleEditClick = () => {
-    setShowEditor(true);
-    document.body.classList.add('edit-mode');
+    setState(BlogContentState.EDIT);
+    document.body.classList.add("edit-mode");
   };
 
   const handlePostSave = (data) => {
     setBlogTitle(data.title);
     setBlogContent(data.body);
-    setShowEditor(false);
-    document.body.classList.remove('edit-mode');
+    setState(BlogContentState.VIEW);
+    document.body.classList.remove("edit-mode");
+  };
+
+  const handleCancel = () => {
+    setState(BlogContentState.VIEW);
+    document.body.classList.remove("edit-mode");
   };
 
   useEffect(() => {
     document.title = `${blogTitle}`;
   }, [blogTitle]);
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape" && state === BlogContentState.EDIT) {
+        handleCancel();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown, { passive: true });
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const handleImageLoad = () => setIsImageLoaded(true);
 
   return (
-    <div className={`blog-content ${showEditor ? 'edit' : ''}`}>
-      {showEditor && (
+    <div className={`blog-content ${state === BlogContentState.EDIT ? "edit" : ""}`}>
+      {state === BlogContentState.EDIT && (
         <div className="edit-form">
-          <div className="edit-form__overlay" onClick={() => setShowEditor(false)} />
+          <div className="edit-form__overlay" onClick={handleCancel} />
           <div className="edit-form__content">
             <div className="edit-form__container">
               <PostEditor
                 blogTitle={blogTitle}
                 content={blogContent}
                 onSave={handlePostSave}
-                onCancel={() => {
-                  setShowEditor(false);
-                  document.body.classList.remove('edit-mode');
-                }}
+                onCancel={handleCancel}
               />
             </div>
           </div>
@@ -53,17 +75,17 @@ const BlogContent = ({ id, title, date, content, image }) => {
         {date && (
           <>
             <span className="blog-content__date">
-              {'Published '}
+              {"Published "}
               <time dateTime={date}>{useDateFormatter(date)}</time>
             </span>
           </>
         )}
-        <FormButton text={'Edit'} onClick={handleEditClick} />
+        <FormButton text={"Edit"} onClick={handleEditClick} />
       </div>
       <h1 className="blog-content__title">{blogTitle}</h1>
       <div className="blog-content__image">
         <img
-          className={` ${isImageLoaded ? 'loaded' : ''}`}
+          className={` ${isImageLoaded ? "loaded" : ""}`}
           src={`/articles/${id}/${image}`}
           alt={title}
           onLoad={handleImageLoad}
@@ -77,6 +99,6 @@ const BlogContent = ({ id, title, date, content, image }) => {
       </section>
     </div>
   );
-};
+});
 
 export default BlogContent;
