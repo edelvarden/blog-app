dfaa4bf34c69720c3941eb43d445ddd6b7919e56
admin <edelvarden@gmail.com>
2023-03-12 00:54:03 +0200

    fix overflow

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from 'react';
-import useDateFormatter from "../../hooks/useDateFormatter";
+import useDateFormatter from '../../hooks/useDateFormatter';
 import FormButton from '../FormButton';
 import PostEditor from '../PostEditor';
 import './styles.scss';
 
 const BlogContent = ({ id, title, date, content, image }) => {
-
   const [showEditor, setShowEditor] = useState(false);
   const [blogTitle, setBlogTitle] = useState(title);
   const [blogContent, setBlogContent] = useState(content);
 
   const handleEditClick = () => {
     setShowEditor(true);
+    document.body.classList.add('edit-mode');
   };
 
   const handlePostSave = (data) => {
     setBlogTitle(data.title);
     setBlogContent(data.body);
     setShowEditor(false);
+    document.body.classList.remove('edit-mode');
   };
 
   useEffect(() => {
@@ -29,24 +30,31 @@ const BlogContent = ({ id, title, date, content, image }) => {
   const handleImageLoad = () => setIsImageLoaded(true);
 
   return (
-    <div className={`blog-content ${showEditor && "edit"}`}>
+    <div className={`blog-content ${showEditor ? 'edit' : ''}`}>
       {showEditor && (
-        <div className='edit-form'>
-          <PostEditor
-            blogTitle={blogTitle}
-            content={blogContent}
-            onSave={handlePostSave}
-            onCancel={() => setShowEditor(false)}
-          />
+        <div className="edit-form">
+          <div className="edit-form__overlay" onClick={() => setShowEditor(false)} />
+          <div className="edit-form__content">
+            <div className="edit-form__container">
+              <PostEditor
+                blogTitle={blogTitle}
+                content={blogContent}
+                onSave={handlePostSave}
+                onCancel={() => {
+                  setShowEditor(false);
+                  document.body.classList.remove('edit-mode');
+                }}
+              />
+            </div>
+          </div>
         </div>
       )}
       <div className="blog-content__header">
         {date && (
           <>
-            <span className="blog-content__date">{'Published '}
-              <time dateTime={date}>
-                {useDateFormatter(date)}
-              </time>
+            <span className="blog-content__date">
+              {'Published '}
+              <time dateTime={date}>{useDateFormatter(date)}</time>
             </span>
           </>
         )}
@@ -55,14 +63,15 @@ const BlogContent = ({ id, title, date, content, image }) => {
       <h1 className="blog-content__title">{blogTitle}</h1>
       <div className="blog-content__image">
         <img
-          className={` ${isImageLoaded && "loaded"}`}
+          className={` ${isImageLoaded ? 'loaded' : ''}`}
           src={`/articles/${id}/${image}`}
           alt={title}
           onLoad={handleImageLoad}
         />
       </div>
       <section className="blog-content__section">
-        <div className="blog-content__content"
+        <div
+          className="blog-content__content"
           dangerouslySetInnerHTML={{ __html: blogContent }}
         ></div>
       </section>
