ae01b99d07f432288dda1b268905c3b56b2da545
edelvarden <edelvarden@gmail.com>
2023-06-08 16:01:05 +0300

    optimize Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,14 @@
+import ToTopButton from "@/components/common/ToTopButton"
 import Footer from "@/components/layout/Footer"
 import Header from "@/components/layout/Header"
-import { clsx } from "clsx"
+import Meta from "@/seo/meta"
+import clsx from "clsx"
 import { Poppins } from "next/font/google"
 import Head from "next/head"
 import { FC, ReactNode } from "react"
-import ToTopButton from "@/components/common/ToTopButton"
+
 import "lazysizes"
 import "lazysizes/plugins/parent-fit/ls.parent-fit"
-import Meta from "@/seo/meta"
 
 const poppins = Poppins({
   weight: ["400", "500", "700"],
@@ -24,12 +25,12 @@ const layoutClassName = clsx(
   poppins.className
 )
 
-interface ILayout {
+interface ILayoutProps {
   title: string
   children: ReactNode
 }
 
-const Layout: FC<ILayout> = ({ title, children }) => {
+const Layout: FC<ILayoutProps> = ({ title, children }) => {
   return (
     <>
       <Meta />
@@ -38,12 +39,10 @@ const Layout: FC<ILayout> = ({ title, children }) => {
       </Head>
       <div className={layoutClassName}>
         <Header routes={[{ path: "/", name: "Blog." }]} />
-        <main className="w-screen flex-1">
-          {children}
-          <ToTopButton />
-        </main>
+        <main className="w-screen flex-1">{children}</main>
         <Footer />
       </div>
+      <ToTopButton />
     </>
   )
 }
